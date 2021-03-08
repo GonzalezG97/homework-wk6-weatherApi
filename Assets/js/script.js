4e4fb0b8873fdbdb433cae57da42a57a81415727
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-03-07 20:32:54 -0600

    handle on user input and main card handles

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,3 +9,21 @@
 
 var apiKey = "67843abc242cc43b51908b533ef15fd4";
 
+// Getting a handle on the user input
+var citySearch = $("#userSearch");
+var searchBtn = $("#searchBtn");
+
+// Getting a handle on the main card items
+var mainCity = $("#citySearch");
+var mainTemp = $("#tempSearch");
+var mainWind = $("#windSearch");
+var mainUv = $("#uvSearch");
+
+
+
+$("#searchBtn").click(function(e){
+e.preventDefault();
+
+
+
+})
