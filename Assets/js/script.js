68763ef942923648fae52ecf654d2d96d8f87324
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-03-07 20:44:19 -0600

    handle on 5day forecast elements

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -19,6 +19,27 @@ var mainTemp = $("#tempSearch");
 var mainWind = $("#windSearch");
 var mainUv = $("#uvSearch");
 
+// Getting a handle on the 5 day forecast items
+var day1 = $("#day1");
+var tempDay1 = $("#tempDay1");
+var humidDay1 = $("#humidDay1");
+
+var day2 = $("#day2");
+var tempDay2 = $("#tempDay2");
+var humidDay2 = $("#humidDay2");
+
+var day3 = $("#day3");
+var tempDay3 = $("#tempDay3");
+var humidDay3 = $("#humidDay3");
+
+var day4 = $("#day4");
+var tempDay4 = $("#tempDay4");
+var humidDay4 = $("#humidDay4");
+
+var day5 = $("#day5");
+var tempDay5 = $("#tempDay5");
+var humidDay5 = $("#humidDay5");
+
 
 
 $("#searchBtn").click(function(e){
